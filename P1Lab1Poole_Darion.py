97c8c9445655cafabdb415232f03f273b71d9801
poolecode85 <pooled4669@student.faytechcc.edu>
2025-04-05 21:12:51 -0400

    Add files via upload

---
+++ P1Lab1Poole_Darion.py
@@ -0,0 +1,14 @@
+#Your Name 
+#Date 
+#Assignment Name 
+#A brief description of the project
+
+First_Name = input("Enter Your First Name")
+Last_Name = input("Enter Your Last Name")
+Date = input("Enter Date")
+Assignment_Name = ("Enter Assignment Name")
+Project_Description = ("Creating scripts with code")
+
+print(f"Hello, {First_Name} {Last_Name}, Welcome to CTI-110")
+
+
