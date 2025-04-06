463086ba67552c553356752eea64c96f3a619f74
poolecode85 <pooled4669@student.faytechcc.edu>
2025-04-05 23:59:46 -0400

    Add files via upload
    2nd upload

---
+++ P1HW_PooleDarion.py
@@ -0,0 +1,36 @@
+# Darion Poole
+# 04/05/2025
+# P1HW1
+# Code expression with intergers
+
+print('------Calculating Exponents------')
+
+print('\n')
+# Get base and exponent vales from user
+
+base = int(input('Enter an interger as a base value: '))
+exponent = int(input('Enter an interger as the exponent: '))
+
+print('\n')
+
+print(base,'raised to the power of', exponent, 'is', base**exponent, '!')
+
+print('\n')
+
+print('------Addition and Subtraction------')
+
+print('\n')
+
+x = int(input('Enter a starting Interger: '))
+y = int(input('Enter an Interger to add: '))
+z = int(input('Enter an Interger to subtract: '))
+
+
+print('\n')
+
+print(x, y, -z, 'Is equal to', x + y - z, '!')
+
+
+
+
+
